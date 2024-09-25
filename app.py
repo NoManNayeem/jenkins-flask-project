e47612e0deea8f8e8d21caa6faf82f36ef1bec33
NoManNayeem <nayeem60151126@gmail.com>
2024-09-25 13:30:33 +0600

    Add logging to Flask app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+import logging
+from flask import Flask
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    app.logger.info("Home page accessed")
+    return "Hello, Jenkins!"
 
 if __name__ == '__main__':
     app.run(debug=True)
